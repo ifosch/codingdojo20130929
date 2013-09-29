3a41665305c4b5a4d17c2f5c6248512b04b0f777
Jare <jarellano@y10k.ws>
2013-09-29 17:59:13 +0200

    cambio a mysqldb en mock

--- katas/jarellano_dacacio.py
+++ katas/jarellano_dacacio.py
@@ -9,36 +9,29 @@ from mock import Mock
 user="juanito"
 passwd = "pepito"
 
-class db (object):
-    query = Mock ()
-    
-class MySQLdb (object):
-
-    bbdd = db()
-    connect = Mock(return_value=bbdd)
 
 class Test(unittest.TestCase):
 
-    def testName(self):
-        
-        pass
-    
-    def testConnect(self):
-        libro1 = llibre ("Titulo1", "Autor1")
-
-        MySQLdb.connect.assert_called_once_with(user,passwd)
-        self.assertIsInstance(MySQLdb.connect(), db)
-        db.query.assert_called_once_with(libro1)
-        
+	def testName(self):
+		
+		pass
+	
+	def testConnect(self):
+		MySQLdb = Mock()
+		MySQLdb.connect = Mock()
+		libro1 = llibre (MySQLdb,"Titulo1", "Autor1")
+		MySQLdb.connect.assert_called_once_with(user,passwd)
+		self.assertIsInstance(MySQLdb.connect(), db)
+		db.query.assert_called_once_with(libro1)
+		
 class llibre(object):
-    
-    
-    def __init__ (self, titol, autor):
-     
-        mydatabase = MySQLdb.connect(user,passwd)
-        mydatabase.query(self)
-    
+		
+	def __init__ (self, _MySQLdb,titol, autor):
+		MySQLdb = _MySQLdb
+		mydatabase = MySQLdb.connect(user,passwd)
+		mydatabase.query(self)
+	
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+	#import sys;sys.argv = ['', 'Test.testName']
+	unittest.main()
